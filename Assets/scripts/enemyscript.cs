8858bc636a7b0fa848eb11b82e51a1e41da687dd
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2024-04-16 16:34:10 +0200

    Create enemyscript.cs

---
+++ Assets/scripts/enemyscript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class enemyscript : MonoBehaviour
+{
+    //Speed and despawn values
+    public float enemyspeed = 10;
+    public float despawnzone = -15;
+
+    // <Variables and references>
+    /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // <Start and Fixed Update>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Call movement call
+        enemymovement();
+    }
+
+    // <Start and Fixed Update>
+    /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // <Functions>
+
+    //Manage movement of enemies
+    void enemymovement()
+    {
+        //Check if enemies is out of bounds 
+        if (transform.position.x > despawnzone)
+        {
+            //Move enemy in X axis
+            transform.position = transform.position + (Vector3.left * enemyspeed) * Time.deltaTime;
+
+        } else
+        {
+            Debug.Log("Enemy despawned");
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
