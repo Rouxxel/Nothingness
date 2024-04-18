f9baeac297024c33f372bd64b0e572a8433030d4
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2024-04-18 14:39:19 +0200

    Update logicscript.cs

--- Assets/scripts/logicscript.cs
+++ Assets/scripts/logicscript.cs
@@ -1,27 +1,89 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class logicscript : MonoBehaviour
 {
+    //Manage time alive 
+    public int score = 0;
+    public Text scoretext;
+
+    //Manage audio sources
+    public AudioSource music;
+    public AudioSource buttonpressed;
+    public AudioSource milestoneachieved;
+
+    //Manage pause game
+    public bool gameispaused = false;
+
+    //Manage UI screens and buttons
+    public GameObject pausedscreen;
+    public GameObject loserscreen;
 
 
-// <Variables and references>
-/// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// <Start and Fixed Update>
+    // <Variables and references>
+    /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // <Start and Fixed Update>
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        music.Play();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    // <Start and Fixed Update>
+    /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // <Functions>
+
+    //Scene management
+    //Function to restart the game
+    public void restartame()
     {
-        
+        Debug.Log("Game restarted");
+        buttonpressed.Play();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
     }
 
-// <Start and Fixed Update>
-/// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// <Functions>
+    //Function to exit the game
+    public void exitgame()
+    {
+        Debug.Log("Game Quitted");
+        buttonpressed.Play();
+        Application.Quit();
+    }
+
+    //Function to pause the game
+    public void pausegame()
+    {
+        Debug.Log("Game paused");
+        buttonpressed.Play();
+        Time.timeScale = 0f;
+        gameispaused = true;
+        music.Pause();
+    }
+
+    public void resumegame()
+    {
+        Debug.Log("Game resumed");
+        buttonpressed.Play();
+        Time.timeScale = 1f;
+        gameispaused = false;
+        music.Play();
+    }
+
+    //Music and sfx management
+    public void musicon()
+    {
+        Debug.Log("Music toggle on");
+        music.volume = 0.1f;
+        buttonpressed.Play();
+    }
+
+    public void musicoff()
+    {
+        Debug.Log("Music toggle off");
+        music.volume = 0f;
+        buttonpressed.Play();
+    }
 
 }
