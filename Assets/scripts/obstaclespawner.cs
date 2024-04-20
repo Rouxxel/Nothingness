324fd30a4255c2eb33d57b48c0d8f47cbd993706
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2024-04-20 15:43:15 +0200

    Update obstaclespawner.cs

--- Assets/scripts/obstaclespawner.cs
+++ Assets/scripts/obstaclespawner.cs
@@ -52,8 +52,8 @@ public class obstaclespawner : MonoBehaviour
             }
         }
 
-        //Limit spawnrate decrease to 1
-        if (spawnrate1 > 1 && playerlogic.playercontrol == true)
+        //Limit spawnrate decrease to 3
+        if (spawnrate1 > 3 && playerlogic.playercontrol == true)
         {
             //Check timer to check if its necessary to decrease spawnrate
             if (decreasetimer < maxtimetodecrease)
